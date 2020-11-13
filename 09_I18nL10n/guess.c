d8f628705f2bb12b221585e3194974b2c4eb77f8
Nikita Mochalov <mochalov.n@yandex.ru>
2020-11-13 18:34:59 +0300

    Add '09_I18nL10n' files

---
+++ 09_I18nL10n/guess.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
+#define LOCALE_PATH "."
+
+int main(void) {
+    setlocale (LC_ALL, "");
+    bindtextdomain ("guess", LOCALE_PATH);
+    textdomain("guess");
+
+    printf(_("Guess a number in the range [1..100]\n"));
+
+    int l = 1;
+    int r = 100;
+    while (l < r) {
+        const int mid = (l + r) / 2;
+        printf(_("Is your number greater than %d? (%s/%s):"), mid, _("y"), _("n"));
+        char buf[10] = {0};
+        int p = scanf("%8s", buf);
+        if (p < 1) {
+            printf(_("Invalid input\n"));
+            exit(1);
+        }
+
+        if (strcmp(buf, _("y")) == 0) {
+            l = mid + 1;
+            continue;
+        }
+
+        if (strcmp(buf, _("n")) == 0) {
+            r = mid;
+            continue;
+        }
+    }
+
+    printf(_("Your number is %d\n"), l);
+
+    return 0;
+}
